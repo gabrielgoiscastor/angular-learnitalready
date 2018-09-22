e0bd2efa06f7f8bdf9ff0b6b280407b1ac9710d1
Gabriel Melo <gabriel@caseimagine.com>
2018-09-22 22:06:20 +0100

    done

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServers = false;
+  serverCreateStatus = 'Nothing...';
 
   constructor() {
 
@@ -19,6 +20,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreateStatus = "Yey!";
   }
 
 }
