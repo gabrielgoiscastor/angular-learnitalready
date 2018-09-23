73aa598bce0b67651fa6157e08c2aca1afa1517e
Gabriel Melo <gabriel@caseimagine.com>
2018-09-23 12:41:42 +0100

    ngFor - nailed it

--- my-first-app/src/app/recipe/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test one', 'Lorem ipsum', 'https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg'),
+    new Recipe('Takito', 'I love this!', 'http://www.fnstatic.co.uk/images/content/recipe/beef-tacos.jpg')
+  ];
 
   constructor() { }
 
